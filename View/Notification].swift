79ae9dae89515f8962aec912bb9163f150c2d6b1
Ekin Bacik <ekinbacik@gmail.com>
2022-09-07 19:16:15 +0300

    notify added

--- View/Notification].swift
+++ View/Notification].swift
@@ -7,29 +7,46 @@
 
 import SwiftUI
 
-struct Notification_: View {
+struct NotfyView: View {
     @State private var testViewModal : ViewModal = ViewModal()
     var body: some View {
         VStack{
-           // ForEach(testViewModal.MustafaKemal){mka in
+            ForEach(testViewModal.MustafaKemal){ mka in
                 Button {
                     let content = UNMutableNotificationContent()
-                    content.title = "that is a test"
-                    content.subtitle = "sa"
-                    let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+                    content.title = "sa"
+                    content.subtitle = "'\(mka.wordVal)'"
+                    let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 4, repeats: false)
                     let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
                     UNUserNotificationCenter.current().add(request)
                 } label: {
-                    Text("R YOOKKKk")
+                    Text("hellow")
                 }
-
-           // }
+            }
+        }
+    }
+}
+struct Permission: View {
+    var body: some View {
+        VStack{
+            Button {
+                let content = UNMutableNotificationContent()
+                content.title = "as"
+                content.subtitle = "as"
+                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 4, repeats: false)
+                let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+                UNUserNotificationCenter.current().add(request)
+            } label: {
+                Text("Permission need")
+            }
+            
         }
     }
 }
 
+
 struct Notification__Previews: PreviewProvider {
     static var previews: some View {
-        Notification_()
+        Permission()
     }
 }
